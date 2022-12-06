5f6ed182fb18d14f3c81f04f26880595de8e00e5
ChadNedzlek <chadnedz@microsoft.com>
2022-12-05 23:09:41 -0800

    Add F# project

---
+++ fsharp/AocF/Program.fs
@@ -0,0 +1,31 @@
+﻿open System.Reflection
+open System.Text.RegularExpressions
+    
+[<EntryPoint>]
+let main args =         
+    let allModules =
+        Assembly.GetExecutingAssembly().GetTypes()
+        |> Seq.filter (fun t -> t.IsAbstract && t.IsSealed && t.IsPublic)
+        |> Seq.filter (fun t -> Regex.IsMatch(t.Name, "problem(\d+)"))
+        |> Seq.sortBy (fun t -> t.FullName[7..])
+        |> Seq.rev
+    
+    let highestModule = allModules |> Seq.head
+    
+    let index = int highestModule.Name[7..]
+    
+    let targetFile = $@"C:\Users\chadnedz\RiderProjects\advent-of-code-2022\aoc\data\data-%02d{index}-real.txt"
+
+    let lines = System.IO.File.ReadLines(targetFile) |> Seq.cache
+    
+    let targetMethod = highestModule.GetMethod("execute")
+    
+    let toExecute =
+        match targetMethod with
+        | method when method.IsGenericMethod -> method.MakeGenericMethod([|lines.GetType()|])
+        | x -> x
+    
+    toExecute.Invoke(null, [|lines|]) |> ignore
+    0
+    
+    

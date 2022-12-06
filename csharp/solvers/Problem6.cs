88a6cda0aec3bf2ba013865ad72d5121c0059a08
ChadNedzlek <chadnedz@microsoft.com>
2022-12-06 01:44:53 -0800

    Remove incorrect license

--- csharp/solvers/Problem6.cs
+++ csharp/solvers/Problem6.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,7 +18,7 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
             }
         }
 
-        private void Run(string line, int size)
+        private static void Run(string line, int size)
         {
             Dictionary<char, int> counts = new();
             for (var i = 0; i < line.Length; i++)
@@ -42,15 +38,15 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
             }
         }
         
-        private void RunLinq(string line, int size)
+        private static void RunLinq(string line, int size)
         {
-            var headerStart = line
+            int headerStart = line
                 .Select((_, i) => line.Substring(i, size))
                 .Select(s => s.Distinct())
                 .Select(s => s.Count())
                 .FindIndex(count => count == size);
 
-            var packetStart = headerStart + size;
+            int packetStart = headerStart + size;
             
             Console.WriteLine($"Found {size} marker at {packetStart} with LINQ");
         }

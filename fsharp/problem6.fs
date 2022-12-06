a034a8bf362fa36556c57a1c59bfeae7343453b6
ChadNedzlek <chadnedz@microsoft.com>
2022-12-06 02:02:28 -0800

    Why be finite? Forever forever!

--- fsharp/problem6.fs
+++ fsharp/problem6.fs
@@ -3,8 +3,8 @@
     let execute(lines) =
         let go (length:int) (line:string) =
             (
-             line
-                |> Seq.mapi (fun i _ -> line[i..(i+length-1)])
+             Seq.initInfinite id
+                |> Seq.map (fun i -> line[i..(i+length-1)])
                 |> Seq.map Seq.distinct
                 |> Seq.map Seq.length
                 |> Seq.findIndex (fun l -> l = length)

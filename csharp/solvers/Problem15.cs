622414bd1f4484fed311b5633bf290875213521f
ChadNedzlek <chadnedz@microsoft.com>
2022-12-14 22:38:37 -0800

    Comments for Day 15

--- csharp/solvers/Problem15.cs
+++ csharp/solvers/Problem15.cs
@@ -46,10 +46,16 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
             Console.WriteLine($"Row contains {total} non-beacon spaces");
         }
 
-        public record WeirdDiagonalBoundingBox(char Id, int Left, int Right, int Top, int Bottom);
+        public record WeirdDiagonalRect(char Id, int Left, int Right, int Top, int Bottom);
         
         private async Task Part2(IAsyncEnumerable<string> data)
         {
+            // We are going to do this whole thing rotated 45 degrees, and we are going to call those
+            // "diagonal coordinates" (or dx,dy).
+            // dx = x + y, dy = x - y
+            // This turns all the areas where mines might be into squares, and we can talk about places that we know there
+            // aren't beacons as "WeirdDiagonalRect", and then we don't need to actually look at each space,
+            // we can just talk about the hypotehtical ranges themselves.
             Stopwatch t = Stopwatch.StartNew();
             char boxId = 'A';
 
@@ -69,77 +75,98 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
             }
 
             var size = 8_000_000;
-            List<WeirdDiagonalBoundingBox> allBoxes = new()
-                { new WeirdDiagonalBoundingBox(boxId, 0, size*2, -size, size) };
+            
+            // To start with we need a rect that can cover the original x/y range of 0-size
+            List<WeirdDiagonalRect> potentialBeaconAreas = new() { new WeirdDiagonalRect(boxId, 0, size*2, -size, size) };
             Dictionary<(int x, int y), char> map = new Dictionary<(int x, int y), char>();
             await foreach (var (sx, sy, bx, by) in Data.As<int, int, int, int>(data,
                                @"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"))
             {
                 
                 int distance = Math.Abs(sx - bx) + Math.Abs(sy - by);
-                var (sdx, sdy) = Translate(sx, sy);
+                var (sdx, sdy) = ToDiagonalCoordinates(sx, sy);
                 int left = sdx - distance - 1;
                 int right = sdx + distance + 1;
                 int top = sdy - distance - 1;
                 int bottom = sdy + distance + 1;
-                List<WeirdDiagonalBoundingBox> newBoxes = new();
-                while (allBoxes.Count > 0)
+                List<WeirdDiagonalRect> newBoxes = new();
+                while (potentialBeaconAreas.Count > 0)
                 {
-                    var slice = allBoxes[0];
-                    allBoxes.RemoveAt(0);
+                    // We need to check each potential beacon area and "slice out" the box of (left,right,top,bottom)
+                    var toSlice = potentialBeaconAreas[0];
+                    potentialBeaconAreas.RemoveAt(0);
 
-                    if (slice.Left > right || slice.Right < left || slice.Bottom < top || slice.Top > bottom)
+                    if (toSlice.Left > right || toSlice.Right < left || toSlice.Bottom < top || toSlice.Top > bottom)
                     {
-                        newBoxes.Add(slice);
+                        newBoxes.Add(toSlice);
                         continue;
                     }
 
-                    if (right <= slice.Right)
+                    // Rip off the sides first
+                    // So if 'a' is the old box, and '.' is what we are removing, we slice off l and r
+                    // aaaaaa      llaarr
+                    // aaaaaa      llaarr
+                    // aa..aa  ==> ll..rr
+                    // aa..aa  ==> ll..rr
+                    // aaaaaa      llaarr
+                    // aaaaaa      llaarr
+                    if (right <= toSlice.Right)
                     {
-                        newBoxes.Add(slice with { Left = right });
+                        newBoxes.Add(toSlice with { Left = right });
                     }
 
-                    if (left >= slice.Left)
+                    if (left >= toSlice.Left)
                     {
-                        newBoxes.Add(slice with { Right = left, Id = BoxId() });
+                        newBoxes.Add(toSlice with { Right = left, Id = BoxId() });
                     }
 
-                    if (top >= slice.Top)
+                    // There are still some top bottom pieces, we need to slice off t and b
+                    // aa      tt
+                    // aa      tt
+                    // ..  ==> ..
+                    // ..  ==> ..
+                    // aa      bb
+                    // aa      bb
+                    if (top >= toSlice.Top)
                     {
-                        newBoxes.Add(new WeirdDiagonalBoundingBox(BoxId(), Math.Max(left + 1, slice.Left),
-                            Math.Min(right - 1, slice.Right), slice.Top, top));
+                        newBoxes.Add(new WeirdDiagonalRect(BoxId(), Math.Max(left + 1, toSlice.Left),
+                            Math.Min(right - 1, toSlice.Right), toSlice.Top, top));
                     }
 
-                    if (bottom <= slice.Bottom)
+                    if (bottom <= toSlice.Bottom)
                     {
-                        newBoxes.Add(new WeirdDiagonalBoundingBox(BoxId(), Math.Max(left + 1, slice.Left),
-                            Math.Min(right - 1, slice.Right), bottom, slice.Bottom));
+                        newBoxes.Add(new WeirdDiagonalRect(BoxId(), Math.Max(left + 1, toSlice.Left),
+                            Math.Min(right - 1, toSlice.Right), bottom, toSlice.Bottom));
                     }
+                    
+                    // We've created new boxes l, r, t, and b, and that accounts for all previous a, and we are ditching
+                    // Z, so we've managed to create up to 4 new boxes, and accounted for all the squares
 
-                    Render(allBoxes.Concat(newBoxes), 20);
+                    Render(potentialBeaconAreas.Concat(newBoxes), 20);
                 }
 
-                newBoxes.RemoveAll(b => BoxSize(b) == 0);
+                // We might have sliced away all the box pieces, so remove any empty ones
+                newBoxes.RemoveAll(b => BoxSize(b) <= 0);
 
-                allBoxes = newBoxes;
+                potentialBeaconAreas = newBoxes;
             }
 
-            var tinyBox = allBoxes.Where(b => BoxSize(b) == 1).ToList();
+            var tinyBox = potentialBeaconAreas.Where(b => BoxSize(b) == 1).ToList();
             foreach (var b in tinyBox)
             {
-                var u = Untranslate(b.Left, b.Top);
+                var u = FromDiagonalCoordinates(b.Left, b.Top);
                 Console.WriteLine(
                     $"Single with frequency ({u.x * 4000000L + u.y}) at (x={u.x}, y={u.y}) (dx={b.Left}, dy={b.Top})");
             }
             Console.WriteLine($"Completed in {t.Elapsed}");
         }
 
-        private static long BoxSize(WeirdDiagonalBoundingBox b)
+        private static long BoxSize(WeirdDiagonalRect b)
         {
             return (b.Right - b.Left + 1) * (long)(b.Bottom - b.Top + 1);
         }
 
-        private static void Render(IEnumerable<WeirdDiagonalBoundingBox> allBoxes, int size)
+        private static void Render(IEnumerable<WeirdDiagonalRect> allBoxes, int size)
         {
             if (Helpers.IncludeVerboseOutput)
             {
@@ -150,7 +177,7 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
                 {
                     for (int x = 0; x <= size; x++)
                     {
-                        var (dx, dy) = Translate(x, y);
+                        var (dx, dy) = ToDiagonalCoordinates(x, y);
                         var box = allBoxes.FirstOrDefault(b => b.Left <= dx && b.Right >= dx && b.Top <= dy && b.Bottom >= dy);
                         if (box != null)
                         {
@@ -167,11 +194,12 @@ namespace ChadNedzlek.AdventOfCode.Y2022.CSharp.solvers
             }
         }
 
-        private static (int dx, int dy) Translate(int x, int y)
+        private static (int dx, int dy) ToDiagonalCoordinates(int x, int y)
         {
             return(x + y, x - y);
         }
-        private static (int x, int y) Untranslate(int dx, int dy)
+        
+        private static (int x, int y) FromDiagonalCoordinates(int dx, int dy)
         {
             return((dx + dy)/2, (dx - dy)/2);
         }
